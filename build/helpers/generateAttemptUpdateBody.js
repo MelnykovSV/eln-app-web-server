8c33d87c7622cc4384499c4a8718b0860f2d1222
MelnykovSV <svmelnykov@gmail.com>
2023-09-25 23:22:11 +0300

    1

--- build/helpers/generateAttemptUpdateBody.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const generateAttemptUpdateBody = (obj) => {
-    const resultingObject = {};
-    for (const item in obj) {
-        resultingObject[`stages.$[].attempts.$[attempt].${item}`] = obj[item];
-    }
-    return resultingObject;
-};
-module.exports = generateAttemptUpdateBody;
