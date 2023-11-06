c9872511cb1d6ff89cf43b724dae1b2ae853dfaf
MelnykovSV <svmelnykov@gmail.com>
2023-11-06 20:09:53 +0200

    styles

--- controllers/auth/verifyEmail.ts
+++ controllers/auth/verifyEmail.ts
@@ -16,9 +16,48 @@ const verifyEmail = async (req: Express.Request, res: Express.Response) => {
     verify: true,
   });
 
-  res.send(
-    `<!DOCTYPE html><html><head><title>Email Validation Success</title><link rel="stylesheet" type="text/css" href="${WEB_SERVER_DOMAIN}/css/styles.css"></head><body><h1>Email Validation Successful</h1><p>Your email has been successfully validated. You can now access our services.</p><p>Here\'s a link to another page:</p><a href=${LOGIN_PAGE_URL}>Login page</a></body></html>`
-  );
+  // res.send(
+  //   `<!DOCTYPE html><html><head><title>Email Validation Success</title><link rel="stylesheet" type="text/css" href="${WEB_SERVER_DOMAIN}/css/styles.css"></head><body><h1>Email Validation Successful</h1><p>Your email has been successfully validated. You can now access our services.</p><p>Login with you new account credentials:</p><a href=${LOGIN_PAGE_URL}>Login page</a></body></html>`
+  // );
+
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Email Validation Success</title>
+        <link rel="stylesheet" type="text/css" href="${WEB_SERVER_DOMAIN}/css/styles.css" />
+      </head>
+      <body>
+        <section class="container section">
+          <div class="logo">
+            <div class="icon-container">
+              <img src="${WEB_SERVER_DOMAIN}/eln_logo.svg" alt="eln logo" />
+            </div>
+            <div class="logo_text-container">
+              <span class="logo_text-line">
+                <span class="logo_capital-letter">E</span>lectronic
+              </span>
+              <span class="logo_text-line">
+                <span class="logo_capital-letter">L</span>aboratory
+              </span>
+              <span class="logo_text-line">
+                <span class="logo_capital-letter">N</span>otebook
+              </span>
+            </div>
+          </div>
+    
+          <h1 class="title">Email Validation Successful</h1>
+          <p class="paragraph outer">
+            Your email has been successfully validated. You can now access our services.
+          </p>
+          <div class="link-paragraph">
+            <p class="paragraph">Login with your new account credentials:</p>
+            <a class="link" href=${LOGIN_PAGE_URL}>Login page</a>
+          </div>
+        </section>
+      </body>
+    </html>
+    `);
 };
 
 module.exports = verifyEmail;
