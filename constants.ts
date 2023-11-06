e2d597c112437c6dfe08e41d43d6e7f49547a245
MelnykovSV <svmelnykov@gmail.com>
2023-11-06 18:25:33 +0200

    styles

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 const LOGIN_PAGE_URL = 'https://eln-app-frontend.vercel.app/login';
-const WEB_SERVER_DOMAIN = 'https://eln-app-frontend.vercel.app';
+const WEB_SERVER_DOMAIN = 'https://eln-app-web-server.onrender.com/';
 
 module.exports = {
   LOGIN_PAGE_URL,
