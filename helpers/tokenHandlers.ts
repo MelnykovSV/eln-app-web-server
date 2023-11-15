bda82ee5146279ea20f74a88e681fa5625c90803
MelnykovSV <svmelnykov@gmail.com>
2023-11-15 21:23:22 +0200

    fixed tokens expiring

--- helpers/tokenHandlers.ts
+++ helpers/tokenHandlers.ts
@@ -6,23 +6,23 @@ const generateTokens = (id: string) => {
   const payload = {
     id,
   };
-  const accessToken = jwt.sign(payload, SECRET_ACCESS_KEY);
-  const refreshToken = jwt.sign(payload, SECRET_REFRESH_KEY);
+  const accessToken = jwt.sign(payload, SECRET_ACCESS_KEY, {
+    expiresIn: '1min',
+  });
+  const refreshToken = jwt.sign(payload, SECRET_REFRESH_KEY, {
+    expiresIn: '30d',
+  });
 
   return { accessToken, refreshToken };
 };
 
 const validateAccessToken = (token: string) => {
-  const result = jwt.verify(token, SECRET_ACCESS_KEY, {
-    expiresIn: '15min',
-  });
+  const result = jwt.verify(token, SECRET_ACCESS_KEY);
   return result;
 };
 
 const validateRefreshToken = (token: string) => {
-  const result = jwt.verify(token, SECRET_REFRESH_KEY, {
-    expiresIn: '30d',
-  });
+  const result = jwt.verify(token, SECRET_REFRESH_KEY);
   return result;
 };
 
